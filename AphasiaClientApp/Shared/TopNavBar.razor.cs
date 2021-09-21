be3c28fe841d346faff4f3024439f09819a65501
Marcin3232 <marcin323239@gmail.com>
2021-09-21 19:24:49 +0200

    initialize bootstrap 5 in the project client

---
+++ AphasiaClientApp/Shared/TopNavBar.razor.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AphasiaClientApp.Shared
+{
+    public partial class TopNavBar
+    {
+    }
+}
