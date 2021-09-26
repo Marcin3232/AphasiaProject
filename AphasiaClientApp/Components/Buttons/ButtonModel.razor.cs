6715c735b66e58f22bee26759b9f4f38f5dfc4bd
Marcin3232 <marcin323239@gmail.com>
2021-09-26 14:38:44 +0200

    add buttons models

---
+++ AphasiaClientApp/Components/Buttons/ButtonModel.razor.cs
@@ -0,0 +1,17 @@
+﻿using AphasiaClientApp.Models.Enums;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AphasiaClientApp.Components.Buttons
+{
+    public partial class ButtonModel
+    {
+        [Parameter]
+        public ButtonTypes ButtonTypes { get; set; }
+        [Parameter]
+        public string Text { get; set; }
+    }
+}
---
+++ AphasiaClientApp/Models/Enums/ButtonTypes.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AphasiaClientApp.Models.Enums
+{
+    public enum ButtonTypes
+    {
+        ButtonSmLight,
+        ButtonSmLDark,
+        ButtonSmLightResponsive,
+        ButtonSmLDarkResponvie,
+        ButtonMdLight,
+        ButtonMdDark,
+        ButtonMdLightResponsive,
+        ButtonMdDarkResponsive,
+        ButtonLgLight,
+        ButtonLgDark,
+        ButtonLgLightResponsive,
+        ButtonLgDarkResponsive
+    }
+}
