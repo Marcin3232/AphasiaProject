f0c9455d9e1df9ae8b4bf5348ecebeb3f530a878
tomacie861 <tomacie861@student.polsl.pl>
2021-10-17 19:16:54 +0200

    add login navigation

---
+++ AphasiaClientApp/Pages/Authentication/Login.razor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AphasiaClientApp.Models.Auth;
+
+namespace AphasiaClientApp.Pages.Authentication
+{
+    public partial class Login
+    {
+        private UserLoginModel userLoginModel = new UserLoginModel();
+
+
+    }
+}
