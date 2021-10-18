aed51499ba23b710410c0e43265ad2a50857ba48
Marcin3232 <marcin323239@gmail.com>
2021-10-18 10:45:46 +0200

    add modal close exercise httml  dialog

--- AphasiaClientApp/Components/Modals/CloseExercisesModal.razor.cs
+++ AphasiaClientApp/Components/Modals/CloseExercisesModal.razor.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Components;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +8,15 @@ namespace AphasiaClientApp.Components.Modals
 {
     public partial class CloseExercisesModal
     {
+
+        [Parameter]
+        public EventCallback<bool> OnClose { get; set; }
+        
+        private Task ModalCancel() =>
+             OnClose.InvokeAsync(false);
+
+        private Task ModalOk() =>
+           OnClose.InvokeAsync(true);
+
     }
 }
