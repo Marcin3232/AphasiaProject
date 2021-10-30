3edd3ff018cec4d61f7f2f945b730c4502e6bbad
Marcin3232 <marcin323239@gmail.com>
2021-10-30 21:47:15 +0200

    main panel exercise anvigation

---
+++ AphasiaClientApp/Components/Cards/CenterNaviCardModel.razor.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace AphasiaClientApp.Components.Cards
+{
+    public partial class CenterNaviCardModel
+    {
+        [Parameter]
+        public string TitleExercise { get; set; }
+        [Parameter]
+        public string TypeTitleExercise { get; set; }
+
+    }
+}
---
+++ AphasiaClientApp/Components/Cards/NaviCardExercise.razor.cs
@@ -0,0 +1,23 @@
+﻿using AphasiaClientApp.Extensions;
+using AphasiaClientApp.Models.Enums;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AphasiaClientApp.Components.Cards
+{
+    public partial class NaviCardExercise
+    {
+        [Parameter]
+        public string Title { get; set; }
+        [Parameter]
+        public bool IsVertical { get; set; }
+        [Parameter]
+        public NaviTypeImage NaviType { get; set; }
+        private string _imageUrl => Path.PathFile + NaviType.GetAttribute<DisplayAttribute>().Name;
+
+    }
+}
