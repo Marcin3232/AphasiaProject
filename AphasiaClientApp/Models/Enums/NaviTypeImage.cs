4953bdfdfecf0ad88f1cd93334e2204d63fee30b
Marcin3232 <marcin323239@gmail.com>
2021-10-30 21:50:53 +0200

    small change top navbar

---
+++ AphasiaClientApp/Models/Enums/NaviTypeImage.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Threading.Tasks;
+
+namespace AphasiaClientApp.Models.Enums
+{
+    public enum NaviTypeImage
+    {
+        [Display(Name = "Back.svg")]
+        Back,
+        [Display(Name = "Close.svg")]
+        Close,
+        [Display(Name = "Helper.svg")]
+        Helper,
+        [Display(Name = "Next.svg")]
+        Next,
+        [Display(Name = "Refresh.svg")]
+        Refresh,
+        [Display(Name = "Sound.svg")]
+        Sound
+    }
+
+    public static class Path
+    {
+        public static string PathFile => @"resource/NaviImage/";
+    }
+}
