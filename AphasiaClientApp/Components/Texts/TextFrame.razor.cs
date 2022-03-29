3e083711fc0e8ca78a2ccd6ec724f3dad4094cf3
Marcin3232 <marcin323239@gmail.com>
2022-03-29 23:28:12 +0200

    rozszerzenie funkconalnosci componentu textFrame

--- AphasiaClientApp/Components/Texts/TextFrame.razor.cs
+++ AphasiaClientApp/Components/Texts/TextFrame.razor.cs
@@ -1,5 +1,6 @@
 ﻿using AphasiaClientApp.Models.Enums;
 using Microsoft.AspNetCore.Components;
+using System.Threading.Tasks;
 
 namespace AphasiaClientApp.Components.Texts
 {
@@ -10,12 +11,39 @@ namespace AphasiaClientApp.Components.Texts
         [Parameter]
         public ColorType Color { get; set; } = ColorType.Normal;
 
+        [Parameter]
+        public bool IsResponsive { get; set; } = false;
+        [Parameter]
+        public bool IsClickable { get; set; } = false;
+        [Parameter]
+        public EventCallback ClickCallback { get; set; }
+
         private string SetBackgroundColors(ColorType color) => color switch
         {
             ColorType.Normal => " bcn ",
             ColorType.Green => " bcg ",
-            ColorType.Red => " bcr "
+            ColorType.Red => " bcr ",
+            ColorType.Light => " bcl ",
+            ColorType.LightEmpty => " bcle ",
+            _ => " ",
         };
 
+        private string SetResponsive(bool isResponsive) => isResponsive ? "auto-size" : "state-size";
+        private string SetPointer => IsClickable ? "cursor:pointer" : " ";
+
+        private string SetAction(ColorType color)
+        {
+            if (IsClickable)
+            {
+                switch (color)
+                {
+                    case ColorType.Normal:
+                        return " bcn-action ";
+                    default:
+                        return " ";
+                }
+            }
+            return " ";
+        }
     }
 }
